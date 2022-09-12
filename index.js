0c511eb184e3ab4b1e3e9c870dd976ebb33667e8
Oliver Dorrian <dorrianolfc@gmail.com>
2022-09-12 15:58:34 +0100

    JS added

--- index.js
+++ index.js
@@ -1,27 +1,184 @@
 let content1_obj = document.getElementById("content-1");
 let content2_obj = document.getElementById("content-2");
-
+let content3_obj = document.getElementById("content-3"); 
+let content4_obj = document.getElementById("content-4");
+let content5_obj = document.getElementById("content-5");
+let content6_obj = document.getElementById("content-6");
+let content7_obj = document.getElementById("content-7");
+let content8_obj = document.getElementById("content-8");
+let content9_obj = document.getElementById("content-9");
+let content10_obj = document.getElementById("content-10");
 
 document.getElementById("content-1-link").addEventListener("click", function () {
-  console.log("content-1: started block");
 
-  // unset all other values
+  // unset all other values content1_obj
+  // skip 1
   content2_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content3_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content4_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content5_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content6_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content7_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content8_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content9_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content10_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
 
   //sets current values
   content1_obj.setAttribute("style", "position: relative !important; display: block !important;");
-
-  console.log("content-1: end block");
 });
 
 
 document.getElementById("content-2-link").addEventListener("click", function () {
-  console.log("content-2: started block"); 
 
-  // unset all other values
+  // unset all other values other than content2_obj
   content1_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  // skip 2
+  content3_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content4_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content5_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content6_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content7_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content8_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content9_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content10_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
 
   //sets current values
   content2_obj.setAttribute("style", "position: relative !important; display: block !important;");
-  console.log("content-2: ran block"); 
+});
+
+document.getElementById("content-3-link").addEventListener("click", function () {
+  // unset all other values other than content3_obj
+  content1_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content2_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  // Skip 3
+  content4_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content5_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content6_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content7_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content8_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content9_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content10_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+
+  //sets current values
+  content3_obj.setAttribute("style", "position: relative !important; display: block !important;");
+});
+
+document.getElementById("content-4-link").addEventListener("click", function () {
+  // unset all other values other than content4_obj
+  content1_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content2_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content3_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  // Skip 4
+  content5_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content6_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content7_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content8_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content9_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content10_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+
+  //sets current values
+  content4_obj.setAttribute("style", "position: relative !important; display: block !important;");
+});
+
+document.getElementById("content-5-link").addEventListener("click", function () {
+  // unset all other values other than content5_obj
+  content1_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content2_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content3_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content4_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  // 5 Skip
+  content6_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content7_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content8_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content9_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content10_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+
+  //sets current values
+  content5_obj.setAttribute("style", "position: relative !important; display: block !important;");
+});
+
+document.getElementById("content-6-link").addEventListener("click", function () {
+  // unset all other values other than content6_obj
+  content1_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content2_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content3_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content4_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content5_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  // Skip 6
+  content7_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content8_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content9_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content10_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+
+  //sets current values
+  content6_obj.setAttribute("style", "position: relative !important; display: block !important;");
+});
+
+document.getElementById("content-7-link").addEventListener("click", function () {
+  // unset all other values other than content6_obj
+  content1_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content2_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content3_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content4_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content5_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content6_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  // Skip 7
+  content8_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content9_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content10_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+
+  //sets current values
+  content7_obj.setAttribute("style", "position: relative !important; display: block !important;");
+});
+
+
+document.getElementById("content-8-link").addEventListener("click", function () {
+  // unset all other values other than content6_obj
+  content1_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content2_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content3_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content4_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content5_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content6_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content7_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  // Skip 8
+  content9_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content10_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+
+  //sets current values
+  content8_obj.setAttribute("style", "position: relative !important; display: block !important;");
+});
+
+document.getElementById("content-9-link").addEventListener("click", function () {
+  // unset all other values other than content6_obj
+  content1_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content2_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content3_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content4_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content5_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content6_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content7_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content8_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  // Skip 9
+  content10_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+
+  //sets current values
+  content9_obj.setAttribute("style", "position: relative !important; display: block !important;");
+});
+
+document.getElementById("content-10-link").addEventListener("click", function () {
+  // unset all other values other than content6_obj
+  content1_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content2_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content3_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content4_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content5_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content6_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content7_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content8_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  content9_obj.setAttribute("style", "visibility: hidden !important; position: absolute !important;");
+  // Skip 10 
+  
+  //sets current values
+  content10_obj.setAttribute("style", "position: relative !important; display: block !important;");
 });
